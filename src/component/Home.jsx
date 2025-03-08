c029728eb54742dd3e77b457f334d4773fd1b107
mustafajam <mustafajamalshabqadar@gmail.com>
2025-03-09 02:17:58 +0500

    add 3D animation in hero section

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Home.css"
 import Navbar from "./Navbar"
 import Card from "./Card"
-import hero from "../assets/hero3.jpeg";
+// import hero from "../assets/hero3.jpeg";
 import img1 from "../assets/sunglass1.jpg";
 import img2 from "../assets/sunglass2.jpg";
 import img3 from "../assets/sunglass3.jpg";
@@ -11,12 +11,12 @@ import img5 from "../assets/sunglass5.jpg";
 import img6 from "../assets/sunglass6.jpg";
 import img7 from "../assets/sunglass7.jpg";
 import img8 from "../assets/sunglass8.jpg";
-import img10 from "../assets/sunglass10.jpeg"
+// import img10 from "../assets/sunglass10.jpeg"
 import Footer from './Foter';
 
 function Home({ addToCart }) {
   const products = [
-    { img: img10, title: "CatEye", price: 25 },
+    { img: img1, title: "CatEye", price: 25 },
     { img: img2, title: "Sunglass 2", price: 30 },
     { img: img3, title: "Sunglass 3", price: 28 },
     { img: img4, title: "Sunglass 4", price: 35 },
@@ -34,9 +34,23 @@ function Home({ addToCart }) {
           <h1>Grab UpTo <span className='spanele'>50%</span> Off On Selected Sun Glasses</h1>
           <button className='herobutt'>Buy Now</button>
         </div>
-        <div className='heroright'>
-          <img src={hero} className='heroimg' alt="Hero" />
+        {/* <div className='heroright'>
+        <img src={hero} className='heroimg' alt="Hero" />
+        </div> */}
+
+        <div className='imgslider3D'>
+        <div className="slider">
+        <div className="slide"><img src={img1} alt="Image 1"/></div>
+        <div className="slide"><img src={img2} alt="Image 2"/></div>
+        <div className="slide"><img src={img3} alt="Image 3"/></div>
+        <div className="slide"><img src={img4} alt="Image 4"/></div>
+        <div className="slide"><img src={img5} alt="Image 5"/></div>
+        
+        
+        
+    </div>
         </div>
+
       </div>
       <h1 style={{ marginLeft: "25%" }}>Sun Glasses For You!</h1>
 
