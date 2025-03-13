ac9d61fed2b7ec49dcee98010ad9d0f4c38906d4
mustafajam <mustafajamalshabqadar@gmail.com>
2025-03-13 15:09:41 +0500

    add some responsivness

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,30 +1,36 @@
-import React,{ useState } from 'react';
-import './Navbar.css'
-import { NavLink } from "react-router-dom";
-
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { FaShoppingCart } from "react-icons/fa";
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div className='nav'>
-      <ul className='navul'>
-
-        <li className='logo'>Pak Shades</li>
-        <li className='navli'><NavLink to="/" >Home</NavLink></li>
-
-        <li><NavLink to="/about" >About</NavLink></li>
-
-        <li><NavLink to="/products" >Products</NavLink></li>
-
-        <li><NavLink to="/cartitem" >Cart Items</NavLink></li>
-
-        <li><NavLink to="/contacts" >Contacts</NavLink></li>
-        
+    <nav className='nav'>
+      <div className='nav-header'>
+        <div className='logo'>Pak Shades</div>
+        <div className='hamburger' onClick={toggleNavbar}>
+          {isOpen ? <FiX size={30} /> : <FiMenu size={50} />}
+        </div>
+      </div>
+      <ul className={isOpen ? 'navul open' : 'navul'}>
+        <li className='navli'><NavLink to='/' onClick={toggleNavbar}>Home</NavLink></li>
+        <li><NavLink to='/about' onClick={toggleNavbar}>About</NavLink></li>
+        <li><NavLink to='/products' onClick={toggleNavbar}>Products</NavLink></li>
+        <li><NavLink to='/cartitem' onClick={toggleNavbar}>Cart Items<span><FaShoppingCart size={25} style={{ position: "relative", top: "4px" }} /></span></NavLink></li>
+        <li><NavLink to='/contacts' onClick={toggleNavbar}>Contacts</NavLink></li>
       </ul>
-    </div>
-  )
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+
+
+
