a32f6ae6ed00c9ab319021c7b6312b86d228895d
mustafajam <mustafajamalshabqadar@gmail.com>
2025-03-17 16:09:33 +0500

    added mui

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pakshades/', // Replace with your GitHub repository name
+  base: '/', // Change this to '/' for Vercel deployment
 });
