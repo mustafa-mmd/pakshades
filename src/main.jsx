7255eb11d6e285484397d3a2708e995222a4bd13
mustafajam <mustafajamalshabqadar@gmail.com>
2025-03-17 16:26:13 +0500

    chn

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/pakshades"> 
+  <BrowserRouter > 
     <App />
   </BrowserRouter>
 );
